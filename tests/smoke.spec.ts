d4f753907b9af9b8137049971cb1768c527ffbcf
Ben Hutchinson <benhutchinson0412@gmail.com>
2026-09-26 10:10:40 +0100

    Update homepage Instagram reel

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -656,7 +656,7 @@ test.describe('public routes', () => {
 
   test('home instagram reel uses the latest requested reel', () => {
     expect(integrationSettings.instagramReelEmbedUrl).toBe(
-      'https://www.instagram.com/reel/DddcwoBMNxs/embed/',
+      'https://www.instagram.com/reel/Ddvdm2aM4VB/embed/',
     );
   });
 
